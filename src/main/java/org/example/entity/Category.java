9b60a7fa00a79d0e6869c443ee5a25643710d3f6
UK <hwangwonuk94@gmail.com>
2024-11-16 06:58:53 +0900

    feat: cascade 적용 전

---
+++ src/main/java/org/example/entity/Category.java
@@ -0,0 +1,41 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> productList = new ArrayList<>();
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addProduct(Product product) {
+        productList.add(product);
+        product.setCategory(this);
+    }
+
+}
